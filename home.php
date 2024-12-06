a8960b5bef3ce8252f4267ec9fa8ae0425c00ab7
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-12-06 16:17:56 +0000

    add css folders

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <title>CareBond - Elder and Patient Caregiver Matching System</title>
     <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="styles1.css">
+    <link rel="stylesheet" href="css styles/styles1.css">
 </head>
 <body>
   <!---------------------title bar----------------------------------------------------------------->
