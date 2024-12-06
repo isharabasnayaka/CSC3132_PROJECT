c5343abf4f433596f3e22f980a5d53fc0e4680ab
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-12-06 16:33:33 +0000

    create header file

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CareBond - Elder and Patient Caregiver Matching System</title>
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css styles/styles1.css">
+</head>
+<body>
+  <!---------------------title bar----------------------------------------------------------------->
+ 
+    <section>
+      <div class="navbar">
+        <p class="logo" style="margin-top: 0px;">Care bond....</p>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#news">Service</a></li>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="joinnow.php">Join now</a></li>
+        </ul>
+      </div>
+</section>
+
+</body>
+</html>
