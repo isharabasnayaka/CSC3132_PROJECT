9449a941f6f871b2cd60c0e3960bb53daf19e7b8
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-12-23 15:31:17 +0000

    change file names and create service css file

--- find care.php
+++ find care.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hire a Nurse Form</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css%20styles/findcare css.css">
+  <link rel="stylesheet" href="cssstyles/findcarecss.css">
 </head>
 <body class="bg-light">
   <!-----------------title bar------------>
--- service.php
+++ service.php
@@ -6,7 +6,7 @@
   <title>Services</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="css codes/service.css">
+  <link rel="stylesheet" href="cssstyles/service.css">
 </head>
 <body>
 <!----------------------------------title bar-------------------------------------------------------------------------------------------->
