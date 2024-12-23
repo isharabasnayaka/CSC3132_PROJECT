aaaad0810649bfb30a40f3200032cb275a003d7d
Ishara Basnayaka <139110442+isharabasnayaka@users.noreply.github.com>
2024-12-23 15:32:58 +0000

    connect to service page for home page

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
         <p class="logo" style="margin-top: 0px;">Care bond....</p>
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#news">Service</a></li>
+          <li><a href="service.php">Service</a></li>
           <li><a href="joinnow.php">Join now</a></li>
           <li><a href="login.php">Login</a></li>
           
